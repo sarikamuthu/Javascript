7ab8227ed3ad95ed46c7c0d7fa68eca56f68804b
Sarika Esackimuthu <87271190+sarikamuthu@users.noreply.github.com>
2023-01-29 22:04:25 +0530

    Add files via upload

---
+++ Ex1.js
@@ -0,0 +1,15 @@
+//Add number to string
+let a="Sarika"
+let b=22
+console.log(a+b)
+console.log(typeof a+b)
+console.log(typeof(a+b))
+
+//add element into cons obj
+const a1={
+    class:"A",
+    subject:"Maths"
+}
+a1['Teacher']="Meena"
+console.log(a1)
+//cannot replicate a1
---
+++ basics.js
@@ -0,0 +1,24 @@
+console.log("sarika")
+food="apple"
+//console.log(food)
+
+//var and let
+//1. var is globally scope while let is not global
+//2. var can be updated and redelcared within scope
+//3. let can be updated but not redeclared
+// ALWAYS USE LET
+
+
+{
+    var a='this'
+    console.log(a)
+}
+console.log(a)
+
+var a='apple'
+{
+    let a='this'
+    console.log(a)
+}
+console.log(a)
+
